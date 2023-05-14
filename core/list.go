2c25cf6f8991c3f551e3fc416b67d50307b272a4
Uuq114 <811670442@qq.com>
2023-05-14 23:42:43 +0800

    fix: add lru list hit

--- core/list.go
+++ core/list.go
@@ -7,18 +7,18 @@ type ListNode struct {
 }
 
 type List struct {
-	dummyHead ListNode
-	dummyTail ListNode
+	dummyHead *ListNode
+	dummyTail *ListNode
 	Length    int64
 }
 
 func (lst *List) Init() {
-	lst.dummyHead = ListNode{
+	lst.dummyHead = &ListNode{
 		Content: NewDummyElem(),
 		Prev:    nil,
 		Next:    nil,
 	}
-	lst.dummyTail = ListNode{
+	lst.dummyTail = &ListNode{
 		Content: NewDummyElem(),
 		Prev:    nil,
 		Next:    nil,
@@ -30,8 +30,16 @@ func (lst *List) Size() int64 {
 	return lst.Length
 }
 
-func (lst *List) Find() string {
-	return "pass"
+func (lst *List) Find(value interface{}) (interface{}, bool) {
+	for iterator := lst.dummyHead.Next; iterator != lst.dummyTail; iterator = iterator.Next {
+		// move node to list head
+		if iterator.Content.Value() == value {
+			lst.remove(iterator)
+			lst.lPushNode(iterator)
+			return iterator.Content.Value(), true
+		}
+	}
+	return nil, false
 }
 
 func (lst *List) lPush(elem string, expiration int64) {
@@ -40,16 +48,20 @@ func (lst *List) lPush(elem string, expiration int64) {
 		Prev:    nil,
 		Next:    nil,
 	}
+	lst.lPushNode(&node)
+}
+
+func (lst *List) lPushNode(node *ListNode) {
 	node.Next = lst.dummyHead.Next
-	lst.dummyHead.Next = &node
-	node.Next.Prev = &node
-	node.Prev = &lst.dummyHead
+	lst.dummyHead.Next = node
+	node.Next.Prev = node
+	node.Prev = lst.dummyHead
 }
 
 func (lst *List) lPop() interface{} {
 	node := lst.dummyHead.Next
 	lst.dummyHead.Next = node.Next
-	node.Next.Prev = &lst.dummyHead
+	node.Next.Prev = lst.dummyHead
 	return node.Content.Value()
 }
 
@@ -59,15 +71,24 @@ func (lst *List) rPush(elem interface{}, expiration int64) {
 		Prev:    nil,
 		Next:    nil,
 	}
-	node.Next = &lst.dummyTail
+	lst.rPushNode(&node)
+}
+
+func (lst *List) rPushNode(node *ListNode) {
+	node.Next = lst.dummyTail
 	node.Prev = lst.dummyTail.Prev
-	node.Prev.Next = &node
-	lst.dummyTail.Prev = &node
+	node.Prev.Next = node
+	lst.dummyTail.Prev = node
 }
 
 func (lst *List) rPop() interface{} {
 	node := lst.dummyTail.Prev
 	lst.dummyTail.Prev = node.Prev
-	node.Prev.Next = &lst.dummyTail
+	node.Prev.Next = lst.dummyTail
 	return node.Content.Value()
+}
+
+func (lst *List) remove(node *ListNode) {
+	node.Prev.Next = node.Next
+	node.Next.Prev = node.Prev
 }
